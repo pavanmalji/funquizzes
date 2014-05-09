2f82dae897a6fac13c569472c48ff7aba79baf06
pavanmalji <pavanmalji@outlook.com>
2014-05-09 18:09:20 +0200

    FB Integration started.

--- utils/authinfo.php
+++ utils/authinfo.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once '../vendor/google/Google_Client.php';
+require_once '../vendor/Facebook/FacebookRedirectLoginHelper.php';
+require_once '../vendor/Facebook/FacebookSession.php';
+require_once '../vendor/Facebook/FacebookSDKException.php';
+require_once '../vendor/Facebook/FacebookRequest.php';
 
 $guest = array(
                 '_id' => '000000000000000000000000',
@@ -28,6 +32,21 @@ if (isset($_GET['gplus'])) {
                              );
          
         $response = json_encode($gplusauthurls, JSON_UNESCAPED_SLASHES);
+    }
+}
+
+if(isset($_GET['fb'])) {
+    if (isset($_GET['authurls'])) {
+        $fbhelper = new Facebook\FacebookRedirectLoginHelper('http://funquizzes.azurewebsites.net/fb-auth-redirect.php', '1484060245157304', 'e17d5c01256c0711d77bd24292ef37ca');
+        
+        $fbauthurls = array(
+                                'provider' => 'fb',
+                                'login' => $fbhelper->getLoginUrl(),
+                                'logout' => '/fb-auth-redirect.php?logout'
+                             );
+         
+        $response = json_encode($fbauthurls, JSON_UNESCAPED_SLASHES);
+        
     }
 }
 
